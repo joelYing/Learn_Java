028f324b41b87407f7ba375d4514126517b3a0b8
joel <Yingjl@suosi.com.cn>
2018-12-05 18:11:44 +0800

    first

---
+++ refirst/src/main/RefirstDemo.java
@@ -0,0 +1,18 @@
+package main;
+
+public class RefirstDemo {
+    public static void main(String[] args) {
+        // float 数据类型需要在末尾加上 f/F
+        float f = 123.456F;
+        // double类型范围大于float
+        double d = 123.456;
+
+        double d1 = 123;
+        double d2 = 124f;
+
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("d1 = " + d1);
+        System.out.println("d2 = " + d2);
+    }
+}
