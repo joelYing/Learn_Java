54e436818253e180938f1fda1cf2c21bb60694d7
joel <Yingjl@suosi.com.cn>
2018-12-13 10:29:10 +0800

    update

---
+++ basic/src/main/ClassDemoDictionary/ClassDemo.java
@@ -0,0 +1,16 @@
+package main.ClassDemoDictionary;
+
+/**
+ * @PackageName main
+ * @Author joel
+ * @Date 2018/12/13 10:07
+ **/
+public class ClassDemo {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.study();
+        student.eat();
+        student.sleep();
+    }
+}
