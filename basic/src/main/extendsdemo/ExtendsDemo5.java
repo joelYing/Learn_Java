c211fc3884a45d2c70675a48df598ed6a3611860
joel <Yingjl@suosi.com.cn>
2018-12-14 11:21:04 +0800

    update

---
+++ basic/src/main/extendsdemo/ExtendsDemo5.java
@@ -0,0 +1,9 @@
+package main.extendsdemo;
+
+/**
+ * @PackageName main.extendsdemo
+ * @Author joel
+ * @Date 2018/12/14 10:47
+ **/
+public class ExtendsDemo5 {
+}
---
+++ basic/src/main/polymorphismpackage/DuoTaiDemo1.java
@@ -0,0 +1,9 @@
+package main.polymorphismpackage;
+
+/**
+ * @PackageName main.polymorphismpackage
+ * @Author joel
+ * @Date 2018/12/14 11:16
+ **/
+public class DuoTaiDemo1 {
+}
