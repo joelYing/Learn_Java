8f0c3917cec3b448d4679a9413a569a46412f7da
joel <Yingjl@suosi.com.cn>
2018-12-28 16:44:14 +0800

    update

---
+++ basic/src/main/java/com/other/other1.java
@@ -0,0 +1,12 @@
+package com.other;
+
+/**
+ * @PackageName com.other
+ * @Author joel
+ * @Date 2018/12/28 16:43
+ **/
+public class other1 {
+    public static void main(String[] args) {
+        System.out.println("空");
+    }
+}
