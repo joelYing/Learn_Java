3ad76f90867000babf18b5dec040f99d2b1d42df
joel <Yingjl@suosi.com.cn>
2019-01-07 17:40:59 +0800

    update

--- basic/src/main/java/com/spiders/FirstSpider.java
+++ basic/src/main/java/com/spiders/FirstSpider.java
@@ -1,30 +1,40 @@
 package com.spiders;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @PackageName com.spiders
  * @Date 2019/1/4 13:43
+ * 爬取IMDB资料库_TOP250_电影 通过 正则 + copyURLToFile
  **/
 public class FirstSpider {
-    public static final String houseInfo = "<div class=\"hong\"><img src=\"(.*?)\".*?>.*?<p class=\\\"bb\\\">(.*?)</p>";
-    public static void getPictures() {
+    public static String House_Info = "<div class=\"hong\"><img src=\"(.*?)\".*?>.*?<p class=\"bb\">(.*?)</p>";
+    public static String userDir = System.getProperties().getProperty("user.dir");
+    public static String DS = File.separator;
+    public static String dirPath = userDir + DS + "data" + DS + "movieimage";
 
+    public static void getPictures(String page) {
         StringBuilder result = new StringBuilder();
+        String line = null;
         BufferedReader br = null;
         try {
-            URL testurl = new URL("http://www.imdb.cn/imdb250/1");
+            URL testurl = new URL("http://www.imdb.cn/imdb250/" + page);
             URLConnection urlConnection = testurl.openConnection();
 
             urlConnection.connect();
             br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            System.out.println(urlConnection.getHeaderFields());
+            // System.out.println(urlConnection.getHeaderFields())
 
-            String line;
             while((line = br.readLine()) != null) {
                 result.append(line);
             }
@@ -39,11 +49,46 @@ public class FirstSpider {
                 e.printStackTrace();
             }
         }
-        System.out.println(result);
+        // System.out.println(result)
+
+        Pattern pattern = Pattern.compile(House_Info);
+        Matcher matcher = pattern.matcher(result);
+        while (matcher.find()) {
+            // matcher.group() 应该是从 1 开始取
+            System.out.println(matcher.group(1) + " : " + matcher.group(2));
+            downLoad(matcher.group(1), matcher.group(2) + ".jpg");
+            System.out.println(matcher.group(2) + " 封面保存成功");
+        }
+    }
+
+    public static void downLoad(String imageUrl, String imageName) {
+        try {
+            URL movieUrl = new URL(imageUrl);
+            File dirFile = new File(dirPath);
+
+            if (!dirFile.exists()) {
+                dirFile.mkdirs();
+            }
+
+            // 此方法可以将任何网址的后台页面内容拷贝到文件内
+            try {
+                FileUtils.copyURLToFile(movieUrl, new File(dirPath + DS + imageName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    public static void getAllPictures() {
+        for (int i = 1; i <= 10; i++) {
+            getPictures(Integer.toString(i));
+        }
+    }
+
     public static void main(String[] args) {
-        getPictures();
+        getAllPictures();
     }
 }
---
+++ basic/src/main/java/com/spiders/SecondSpider.java
@@ -0,0 +1,9 @@
+package com.spiders;
+
+/**
+ * @PackageName com.spiders
+ * @Author joel
+ * @Date 2019/1/7 16:59
+ **/
+public class SecondSpider {
+}
