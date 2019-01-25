acaf4b8a1364fad83e90f0f4c4e55575a7a77a39
joel <Yingjl@suosi.com.cn>
2019-01-25 18:09:06 +0800

    spring

--- spring/spring/src/springjdbc/SpringJdbcTest.java
+++ spring/spring/src/springjdbc/SpringJdbcTest.java
@@ -1,7 +1,11 @@
 package springjdbc;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @PackageName springjdbc
@@ -9,13 +13,17 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
  * @Date 2019/1/25 16:22
  **/
 public class SpringJdbcTest {
-    public static void run1() {
+    /**
+     * 相对原始的方法
+     */
+    private static void run1() {
         // 使用Spring内置的连接池
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 
         // jdbc:mysql:/// 等价于 jdbc:mysql://localhost:3306
-        dataSource.setUrl("jdbc:mysql:///springjdbc");
+        // The server time zone value 'ÖÐ¹ú±ê×¼Ê±¼ä' is unrecognized or represents 时区问题
+        dataSource.setUrl("jdbc:mysql:///springjdbc?serverTimezone=GMT%2B8");
         dataSource.setUsername("root");
         dataSource.setPassword("123456");
 
@@ -23,7 +31,11 @@ public class SpringJdbcTest {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
         // 数据添加
-        jdbcTemplate.update("insert into t_account values (null,?,?)", "Spring", 998);
+        jdbcTemplate.update("insert into t_account values (14,?,?)", "Spring", 998.0);
+    }
+
+    private static void run2() {
+        
     }
 
     public static void main(String[] args) {
