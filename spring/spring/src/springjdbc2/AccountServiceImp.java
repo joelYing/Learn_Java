97baff645c40a277d549b7e878c50eae819b87f9
joel <Yingjl@suosi.com.cn>
2019-01-28 15:36:30 +0800

    spring

--- spring/spring/src/springjdbc2/AccountServiceImp.java
+++ spring/spring/src/springjdbc2/AccountServiceImp.java
@@ -1,12 +1,14 @@
 package springjdbc2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @PackageName springjdbc2
  * @Author joel
  * @Date 2019/1/28 12:17
  **/
+@Transactional
 public class AccountServiceImp implements AccountService{
     @Autowired
     private AccountDao accountDao;
