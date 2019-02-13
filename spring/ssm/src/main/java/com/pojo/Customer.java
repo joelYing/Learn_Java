13ed3c86c3e540409c0fbf44c4efd1358ae93fce
joel <Yingjl@suosi.com.cn>
2019-02-13 18:23:26 +0800

    spring

---
+++ spring/ssm/src/main/java/com/pojo/Customer.java
@@ -0,0 +1,56 @@
+package com.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @PackageName com.pojo
+ * @Author joel
+ * @Date 2019/1/30 11:36
+ **/
+public class Customer implements Serializable {
+    private long id;
+    private String name;
+    private String telephone;
+    private String address;
+    private String remark;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
